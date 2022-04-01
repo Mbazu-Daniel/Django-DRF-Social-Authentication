67e5985d38179dca9db8cfba3f140c9ae968bd72
Mbazu Daniel <mbazudaniel97@gmail.com>
2022-04-01 13:59:27 +0100

    secret key

--- base/settings.py
+++ base/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cdw!0jds5be63&n6=*f19$@)6me$8^&)ow*285k+1uotxjew^2'
+SECRET_KEY = config(SECRET_KEY, cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
