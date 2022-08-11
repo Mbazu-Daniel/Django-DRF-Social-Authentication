11d07eae58fc39f6a57f15ac101cfb45bd7f53ee
Mbazu Daniel <mbazudaniel97@gmail.com>
2022-08-11 23:17:56 +0100

    adding https:// to corsheaders address

--- base/settings/common.py
+++ base/settings/common.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 
 INTERNAL_IPS = ["127.0.0.1"]
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8001", "http://localhost:8001", "bizka.herokuapp.com", "bizka.render.com"]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8001", "http://localhost:8001", "https://bizka.herokuapp.com", "https://bizka.render.com"]
 
 
 ROOT_URLCONF = "base.urls"
