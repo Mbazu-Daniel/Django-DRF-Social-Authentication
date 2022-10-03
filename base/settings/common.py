a0167debc91ef3fb67d8737f280be86f92a10fae
Mbazu Daniel <Mbazudaniel97@gmail.com>
2022-10-03 12:45:20 +0100

    set cors headers to general

--- base/settings/common.py
+++ base/settings/common.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 
 INTERNAL_IPS = ["127.0.0.1"]
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8001", "http://localhost:8001", "http://127.0.0.1:3000", "http://localhost:3000", "https://bizka.herokuapp.com", "https://bizka.render.com", "https://bizka.vercel.app"]
+
 
 
 ROOT_URLCONF = "base.urls"
--- base/settings/prod.py
+++ base/settings/prod.py
@@ -8,6 +8,8 @@ ALLOWED_HOSTS = ["*"]
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
+# CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8001", "http://localhost:8001", "http://127.0.0.1:3000", "http://localhost:3000", "https://bizka.herokuapp.com", "https://bizka.render.com", "https://bizka.vercel.app"]
+CORS_ALLOWED_ORIGINS = ["*"]
 
 DATABASES = {
     "default": dj_database_url.config()
