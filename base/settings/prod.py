de9efb1b42430cb19122a5878757a9279e130c57
Mbazu Daniel <Mbazudaniel97@gmail.com>
2022-10-12 14:47:34 +0100

    add demoobizzy to cors origins

--- base/settings/prod.py
+++ base/settings/prod.py
@@ -9,7 +9,7 @@ ALLOWED_HOSTS = ["*"]
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8001", "http://localhost:8001", "http://127.0.0.1:3000", "http://localhost:3000", "https://bizka.herokuapp.com", "https://bizka.render.com", "https://bizka.vercel.app"]
-CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOWED_ORIGINS = ["*", "https://demobizzy.vercel.app"]
 
 DATABASES = {
     "default": dj_database_url.config()
